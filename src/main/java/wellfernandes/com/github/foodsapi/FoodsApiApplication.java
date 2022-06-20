2f3d23cdf524d7412206e2122212412c5d2720ce
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-20 12:37:33 -0300

    project created

---
+++ src/main/java/wellfernandes/com/github/foodsapi/FoodsApiApplication.java
@@ -0,0 +1,13 @@
+package wellfernandes.com.github.foodsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wellfernandes/com/github/foodsapi/FoodsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package wellfernandes.com.github.foodsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

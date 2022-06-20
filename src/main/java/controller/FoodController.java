cb4783ea941ca78e596ef4c6aca2e4a889452b54
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-20 18:50:03 -0300

    implemented create and update food

--- src/main/java/controller/FoodController.java
+++ src/main/java/controller/FoodController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +37,21 @@ public class FoodController {
 			e.printStackTrace();
 			return ResponseEntity.notFound().build();
 		}
+	}
+
+	@PostMapping
+	public Food createFood(@RequestBody Food food) {
+		return foodServices.saveFood(food);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Food> updateFood(@PathVariable Long id, @RequestBody Food food) {
+		try {
+			Food updatedFood = foodServices.updateFood(id, food);
+			return ResponseEntity.ok(updatedFood);
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+
 	}
 }

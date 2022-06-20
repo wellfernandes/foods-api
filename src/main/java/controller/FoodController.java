34a56cae465d1e9da4fd173b92cda4b83e25174e
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-20 18:41:26 -0300

    implemented list all and get food by id

--- src/main/java/controller/FoodController.java
+++ src/main/java/controller/FoodController.java
@@ -1,9 +1,15 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import services.FoodServices;
+import wellfernandes.com.github.foodsapi.model.Food;
 
 @RestController
 @RequestMapping("/foods")
@@ -13,5 +19,21 @@ public class FoodController {
 
 	public FoodController(FoodServices foodServices) {
 		this.foodServices = foodServices;
+	}
+
+	@GetMapping
+	public List<Food> listAll() {
+		return foodServices.getAllFoods();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Food> getFoodById(@PathVariable Long id) {
+		try {
+			Food food = foodServices.getFoodById(id);
+			return ResponseEntity.ok(food);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
